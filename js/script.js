e27fe9ad5c55ba2a36b6c9412d81686f638946ef
Pawel Walicki <pawel.walicki89@interia.pl>
2022-09-04 19:15:49 +0200

    added function to JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-let converterElement = document.querySelector(".js-PLN");
-let formElement = document.querySelector(".js-form");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const converterElement = document.querySelector(".js-PLN");
+    const currencyElement = document.querySelector(".js-currency");
+    const formElement = document.querySelector(".js-form");    
+    const resultElement = document.querySelector(".js-result");
 
-
-
-formElement.addEventListener("input", () => {
-    let amount = converterElement.value;
-    let currency = currencyElement.value;
-
-    let result = amount / currency;
-
-    resultElement.innerText = result.toFixed(2);
-});
+    const caltulateResult = () => {
+        formElement.addEventListener("input", () => {
+            const amount = converterElement.value;
+            const currency = currencyElement.value;
+            const result = amount >= 0 ? amount / currency : 0;
+            resultElement.innerText = result.toFixed(2);
+        })
+    };
+    caltulateResult();
+}
